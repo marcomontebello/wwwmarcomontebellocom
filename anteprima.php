9f633aa12f71cce6e1ec5b747fd3d14db0f31d00
marcomontebello <marcomontebello@live.co>
2020-06-14 16:57:16 +0200

    wordpress installation

--- anteprima.php
+++
@@ -1,5 +0,0 @@
-<?php
-function anteprima($testo, $lunghezza, $finale) {
-return (count($parole = explode(' ', $testo)) > $lunghezza) ? implode(' ', array_slice($parole, 0, $lunghezza)) . $finale : $testo;
-}
-?>
